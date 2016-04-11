6524ad5c458aa14e06190b65d393bb4403c93cfb
abeosoft <abeosoft@gmail.com>
2016-04-11 18:18:34 -0400

    added zuul service proxy, eureka service registry

---
+++ service-proxy/src/main/java/com/abeosoft/microservices/platform/ServiceProxyApplication.java
@@ -0,0 +1,12 @@
+package com.abeosoft.microservices.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProxyApplication.class, args);
+	}
+}
---
+++ service-proxy/src/test/java/com/abeosoft/microservices/platform/ServiceProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abeosoft.microservices.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ServiceProxyApplication.class)
+public class ServiceProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
