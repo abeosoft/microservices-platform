a0726c394c2a26239fbe1ccfff95c8f185a0e1aa
abeosoft <abeosoft@gmail.com>
2016-04-12 00:05:23 -0400

    in progress: service registry and service proxy

--- service-proxy/src/main/java/com/abeosoft/microservices/platform/ServiceProxyApplication.java
+++ service-proxy/src/main/java/com/abeosoft/microservices/platform/ServiceProxyApplication.java
@@ -2,8 +2,14 @@ package com.abeosoft.microservices.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@Controller
 public class ServiceProxyApplication {
 
 	public static void main(String[] args) {
